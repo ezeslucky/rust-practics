ec6c84c46b4aa68b490858dae1f180d3ba81657e
Lucky <lucky123@gmail.com>
2025-06-22 10:17:43 +0530

    is_even

---
+++ is_even/target/debug/deps/is_even.d
@@ -0,0 +1,5 @@
+C:\Users\aurob\ALL CODING\100Xdevs-Project\rust\practics\is_even\target\debug\deps\is_even.exe: src\main.rs
+
+C:\Users\aurob\ALL CODING\100Xdevs-Project\rust\practics\is_even\target\debug\deps\is_even.d: src\main.rs
+
+src\main.rs:
---
+++ is_even/target/debug/is_even.d
@@ -0,0 +1,1 @@
+C:\Users\aurob\ALL\ CODING\100Xdevs-Project\rust\practics\is_even\target\debug\is_even.exe: C:\Users\aurob\ALL\ CODING\100Xdevs-Project\rust\practics\is_even\src\main.rs
